8ba8dc0710ac9fe537376a3b53200baa394375d8
luxagen <hello@luxagen.com>
2018-02-11 12:03:23 +0000

    filter-smudge: refactor

--- lib/git-media/filter-smudge.rb
+++ lib/git-media/filter-smudge.rb
@@ -3,59 +3,55 @@ require 'git-media/helpers'
 module GitMedia
   module FilterSmudge
 
+    def self.ensure_exists(sha)
+      sha.enforce_hash
+
+      cache_file = GitMedia.media_path(sha)
+
+      return cache_file if File.exist?(cache_file) # Early exit if the object is already cached
+
+      # Read key from config
+      auto_download = `git config git-media.autodownload`.chomp.downcase == "true"
+
+      unless auto_download
+        STDERR.puts('Object missing, writing placeholder: ' + sha)
+        return nil
+      end
+
+      STDERR.puts ("Downloading: " + sha[0,8])
+      pull = GitMedia.get_pull_transport
+      pull.pull(nil, sha) # nil because this filter has no clue what file stdout will be piped into
+
+      unless File.exist?(cache_file)
+        STDERR.puts ("Could not get object, writing stub : " + sha)
+        return nil
+      end
+
+      return cache_file
+    end
+
     def self.run!
-      media_buffer = GitMedia.get_media_buffer
-      
-      # read checksum size
       STDIN.binmode
       STDOUT.binmode
-      orig = STDIN.readline(64)
 
-      if sha = orig.stub2hash
-        # this is a media file
-        media_file = File.join(media_buffer, sha)
-        if File.exists?(media_file)
-          STDERR.puts('Recovering media : ' + sha)
-          File.open(media_file, 'rb') do |f|
-            unless GitMedia::Helpers.copy(STDOUT, f) return 1
-          end
-        else
-          # Read key from config
-          auto_download = `git config git-media.autodownload`.chomp.downcase == "true"
+      prefix = STDIN.read(GM_BUFFER_BYTES)
 
-          if auto_download
-
-            pull = GitMedia.get_pull_transport
-
-            cache_file = GitMedia.media_path(sha)
-            if !File.exist?(cache_file)
-              STDERR.puts ("Downloading : " + sha[0,8])
-              # Download the file from backend storage
-              # We have no idea what the final file will be (therefore nil)
-              pull.pull(nil, sha)
-            end
-
-            if File.exist?(cache_file)
-              STDERR.puts ("Expanding : " + sha[0,8])
-              File.open(media_file, 'rb') do |f|
-                unless GitMedia::Helpers.copy(STDOUT, f) return 2
-              end
-            else
-              STDERR.puts ("Could not get object, writing placeholder : " + sha)
-              puts orig
-            end
-
-          else
-            STDERR.puts('Object missing, writing placeholder : ' + sha)
-            # Print orig and not sha to preserve eventual newlines at end of file
-            # To avoid git thinking the file has changed
-            puts orig
-          end
-        end
-      else
-        # if it is not a 40 character long hash, just output
+      unless sha = prefix.stub2hash
+        # If the file isn't a stub, just pass it through
         STDERR.puts('Unknown git-media stub format')
-        unless GitMedia::Helpers::copy(STDOUT,STDIN,orig) return 3
+        GitMedia.Helpers.copy(STDOUT,STDIN,prefix)
+        return 0
+      end
+
+      unless cache_file = ensure_exists(sha)
+        print prefix # Pass stub through
+        return 0
+      end
+
+      # Reaching here implies that cache_file exists
+      STDERR.puts ("Expanding : " + sha[0,8])
+      File.open(cache_file, 'rb') do |f|
+        GitMedia::Helpers.copy(STDOUT,f)
       end
 
       return 0
