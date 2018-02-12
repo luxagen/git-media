b71842798c264cb2caa3c7049b9da7056659b31b
luxagen <hello@luxagen.com>
2018-02-12 10:46:18 +0000

    clear: improvements

--- lib/git-media/clear.rb
+++ lib/git-media/clear.rb
@@ -9,14 +9,14 @@ module GitMedia
     end
     
     def self.clear_local_cache
-      # find files in media buffer and delete all pushed files
+      # find files in cache and delete all pushed files
       all_cache = Dir.chdir(GitMedia.cache_path) { Dir.glob('*') }
       unpushed_files = @push.get_unpushed(all_cache)
-      pushed_files = all_cache - unpushed_files
-      pushed_files.each do |hash|
-        hash.enforce_sha1
-        puts "Removing " + hash[0, 8] + " from cache"
-        File.unlink(File.join(GitMedia.cache_path, hash))
+      pushed_files = all_cache-unpushed_files
+      pushed_files.each do |name|
+        next unless name.hash?
+        puts "#{name}: removed from cache"
+        File.unlink(File.join(GitMedia.cache_path, name))
       end
     end
     
