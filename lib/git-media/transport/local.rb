de7890958b2b16395c696f8fca26fbf30ee013f4
luxagen <hello@luxagen.com>
2018-02-18 12:10:23 +0000

    refactors and fixes

--- lib/git-media/transport/local.rb
+++ lib/git-media/transport/local.rb
@@ -11,6 +11,11 @@ module GitMedia
   module Transport
     class Local < Base
 
+      def error_inaccessible
+        STDERR.puts "local store '#{@path}' is inaccessible"
+        exit 1
+      end
+
       def initialize
         @path = `git config git-media.localpath`.chomp
         if @path === ""
@@ -19,26 +24,38 @@ module GitMedia
       end
 
       def read(hash)
-        return File.open(File.join(@path, hash), 'rb') do |istr|
-          STDERR.puts "before yield"
-          value = yield istr
-          STDERR.puts "after yield"
-          next value
+        error_inaccessible unless Dir.exist?(@path)
+
+        begin
+          return File.open(File.join(@path, hash), 'rb') do |istr|
+            STDERR.puts "before yield"
+            value = yield istr
+            STDERR.puts "after yield"
+            next value
+          end
+        rescue
+          STDERR.puts "#{hash}: remote object inaccessible"
         end
 
         return false
       end
 
       def write(hash)
+        error_inaccessible unless Dir.exist?(@path)
+
         temp = File.join(@path,'obj.temp')
 
         result=false
 
-        result = File.open(temp,'wb') do |ostr|
-          STDERR.puts 'before yield'
-          value = yield ostr
-          STDERR.puts 'after yield'
-          next value
+        begin
+          result = File.open(temp,'wb') do |ostr|
+            STDERR.puts 'before yield'
+            value = yield ostr
+            STDERR.puts 'after yield'
+            next value
+          end
+        rescue
+          STDERR.puts "#{hash}: unable to create remote object"
         end
 
         FileUtils.mv(temp,File.join(@path, hash),:force => true) if result
@@ -48,10 +65,7 @@ module GitMedia
       def list(intersect,excludeFrom)
         upstream =  `ls #{@path}/ -1ap 2>/dev/null`.split("\n").select { |f| f.match(GM_HASH_REGEX) }.to_set
 
-        if 0 != $?.exitstatus
-          STDERR.puts "local store '#{@path}' is inaccessible" 
-          exit 1
-        end
+        error_inaccessible if 0 != $?.exitstatus
 
         intersected  =  intersect ? upstream&intersect : upstream
 
