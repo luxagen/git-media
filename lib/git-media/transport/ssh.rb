19d1773f3f794075244254104e43f027bc5eb31b
luxagen <hello@luxagen.com>
2018-02-20 15:12:19 +0000

    renamed SCP -> SSH

--- lib/git-media/transport/ssh.rb
+++ lib/git-media/transport/ssh.rb
@@ -2,33 +2,35 @@ require 'git-media/transport'
 
 require 'set'
 
-# Implements object store at an SCP path
+# Implements object store at an SSH path
 
-# git-media.transport scp
-# git-media.scpuser someuser
-# git-media.scphost remoteserver.com
-# git-media.scppath /opt/media
+# git-media.transport ssh
+# git-media.user someuser
+# git-media.host remoteserver.com
+# git-media.path /opt/media
 
 module GitMedia
   module Transport
-    class Scp < Base
+    class SSH < Base
 
       def initialize
-        @path = `git config git-media.scppath`.chomp
+        @path = `git config git-media.path`.chomp
         if ''===@path
-          raise "git-media.scppath not set for scp transport"
+          raise "git-media.path not set for SSH transport"
         end
 
-        user = `git config git-media.scpuser`.chomp
-        host = `git config git-media.scphost`.chomp
-        port = `git config git-media.scpport`.chomp
+        user = `git config git-media.user`.chomp
+        host = `git config git-media.host`.chomp
         if ''===user
-          raise "git-media.scpuser not set for scp transport"
+          raise "git-media.user not set for SSH transport"
         end
         if ''===host
-          raise "git-media.scphost not set for scp transport"
+          raise "git-media.host not set for SSH transport"
         end
+
+        port = `git config git-media.port`.chomp
         portsw  =  ''==port ? '' : " -p#{port}"
+
         @sshcmd = "ssh#{portsw} #{user}@#{host}"
       end
 
